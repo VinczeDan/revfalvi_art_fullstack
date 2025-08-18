8c519cff2b5552195823fdaf667c41c0d4fc6721
VinczeDan <vincze.daniel.22f@szlgbp.hu>
2025-08-18 13:20:50 +0200

    cont3

--- backend/settings.py
+++ backend/settings.py
@@ -115,8 +115,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # settings.py
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 feEMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = 'daniel.vincze15@gmail.com'
 EMAIL_HOST_PASSWORD = 'mthg vhcu utjb zycg'  # Ellenőrizd újra az App Password-t
 DEFAULT_FROM_EMAIL = 'daniel.vincze15@gmail.com'
