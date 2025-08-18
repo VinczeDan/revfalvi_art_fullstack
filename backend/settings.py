0d240064e117e259b8c43a050054ecf2bddd6841
VinczeDan <vincze.daniel.22f@szlgbp.hu>
2025-08-18 12:59:09 +0200

    cont2

--- backend/settings.py
+++ backend/settings.py
@@ -110,7 +110,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # settings.py
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+feEMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'daniel.vincze15@gmail.com'
