18da54cded55301f279b8939cc0927f59bb69bfd
daniel.vincze15@gmail.com <daniel.vincze15@gmail.com>
2025-10-01 12:01:57 +0200

    email3

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,12 @@
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from django.urls import path
+from .views import PaintingViewSet, TodoViewSet, send_contact_email, NewsViewSet
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/", include("api.urls")),  # csak itt kell behúzni az api.urls-t
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
-  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+router = DefaultRouter()
+router.register(r'paintings', PaintingViewSet)
+router.register(r'todos', TodoViewSet)
+router.register(r'news', NewsViewSet)
+
+urlpatterns = router.urls + [
+    path("send-contact-email/", send_contact_email, name="send_contact_email"),
+]
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -5,6 +5,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("api.urls")),  # csak itt kell behúzni az api.urls-t
+    path("api/", include("api.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
