9bdf51d4cc4348028ca69847ceacff442405a647
daniel.vincze15@gmail.com <daniel.vincze15@gmail.com>
2025-10-01 11:59:59 +0200

    email2

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,19 +1,10 @@
-from rest_framework.routers import DefaultRouter
-from .views import PaintingViewSet, TodoViewSet, send_contact_email, NewsViewSet
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
-
-router = DefaultRouter()
-router.register(r'paintings', PaintingViewSet)
-router.register(r'todos', TodoViewSet)
-router.register(r'news', NewsViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("api.urls")),
+    path("api/", include("api.urls")),  # csak itt kell behúzni az api.urls-t
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,11 +2,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
 
-from api.views import PaintingViewSet, TodoViewSet, send_contact_email, NewsViewSet
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+    path("admin/", admin.site.urls),
+    path("api/", include("api.urls")),  # csak itt kell behúzni az api.urls-t
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
