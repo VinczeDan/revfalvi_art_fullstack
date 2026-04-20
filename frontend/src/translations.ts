dbb40f069abe56b39d7979f5fedc5afce3834bfa
VinczeDan <vincze.daniel.22f@szlgbp.hu>
2026-04-20 19:19:09 +0200

    rovidebb

--- frontend/src/translations.ts
+++ frontend/src/translations.ts
@@ -20,29 +20,19 @@ export const translations = {
       cardTitle: "Küldetésnyilatkozat",
       cardContent: `Kedves Nyelvtanuló! 
 
-
-
 Révfalvi Péter vagyok, végzettségemet tekintve angol nyelvtanár. 
-
 
 Jó helyen jársz, ha segítségre van szükséged az angol nyelv tanulásában. Sok-sok évvel ezelőtt, én is kerestem azt a lehetőséget, azt a személyt, aki segített eligazodni egy idegen nyelv használatában. Tanultam a suliban, de igazából nem kaptam 100%-os figyelmet. A csoportbontásról már ne is beszéljünk. Túl sokan voltunk, és tudásunk nagyon eltérő volt. 
 
-
 Jártam tanfolyamra is, ahol már sokkal jobban éreztem magamat, és meg mertem szólalni angolul. Kiscsoportban tanultunk, a tanárnak sokkal több ideje volt rám figyelni. Igazán motiváló volt! 
-
-
 
 Ezután vettem egyéni órákat is. Azt gondolom, hogy itt kaptam meg az a teljes figyelmet, amire mindig is vágytam nyelvtanulóknak. Emlékszem Tomra, aki amerikai volt. Ő tanított meg jó néhány kifejezésre, amire mai napig emlékszem. 
 
-
 Majd jött az Egyesült Királyság. Ez volt az igazi ,,mélyvíz”! Éveken keresztül volt lehetőségem Londonban, anyanyelvi területen hallgatni és beszélni az angolt. Ez volt a csúcsok csúcsa! Nem bántam meg!!! 
-
 
 Majd hazajöttem, és azóta itthon segítek azokat, akik szeretnének egy versenyképesebb angol nyelvtudással rendelkezni. 
 
-
 Hogy mit kapsz a pénzedért cserébe? 
-
 
 100%-os figyelmet, egyéni szükségletek figyelembe vételét, biztatást, és sok-sok segítséget, ha valamit nem értesz! `,
     },
